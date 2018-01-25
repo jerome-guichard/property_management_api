400309515c72c6ec6c32bae9ac682f5f44baaf31
jerome-guichard <guichard_jerome@yahoo.fr>
2018-01-25 23:51:00 +0100

    User Model: Add email key

--- app/models/user.py
+++ app/models/user.py
@@ -5,15 +5,19 @@ class User(db.Model):
     
     # id (Integer)
     id = db.Column(db.Integer, primary_key=True)
-    # User firstname (limited to 80 chars)
-    firstname = db.Column(db.String(80), unique=False)
-    # User lastname (limited to 80 chars)
-    lastname = db.Column(db.String(80), unique=False)
-    # User birthday (Date time)
-    birthday = db.Column(db.DateTime)
+    # User firstname (limited to 120 chars) - Required
+    firstname = db.Column(db.String(120), unique=False,nullable= False)
+    # User lastname (limited to 120 chars) - Required
+    lastname = db.Column(db.String(120), unique=False,nullable= False)
+    # User birthday (Date time) - Required
+    birthday = db.Column(db.DateTime,unique=False, nullable= False)
+    # Email must be unique - Required - Unique
+    email = db.Column(db.String(120))
     
     # User Constructor
-    def __init__(self, firstname, lastname,birthday):
+    def __init__(self, firstname, lastname,birthday,email):
         self.firstname = firstname
         self.lastname = lastname
         self.birthday = birthday
+        self.email = email
+        
