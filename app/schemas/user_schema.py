7eee04e58b5542516cfc83c5b4ea318004fb5d91
jerome-guichard <guichard_jerome@yahoo.fr>
2018-01-25 23:52:21 +0100

    Update User Schema with email field

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,7 +1,9 @@
 from app import ma
+from app.models.user import User
 
 # Schema for DB (structure of endpoints responses)
 class UserSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('firstname', 'lastname','birthday')
+        model = User
+        fields = ('firstname', 'lastname','birthday','email')    
