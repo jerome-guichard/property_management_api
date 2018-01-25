1ac6bf9df8d2634c8d2c7588781a0fcabf4cd4c5
jerome-guichard <guichard_jerome@yahoo.fr>
2018-01-26 00:31:52 +0100

    Remove unused import

--- app/schemas/property_schema.py
+++ app/schemas/property_schema.py
@@ -1,6 +1,8 @@
 from app import ma
+from app.models.property import Property
+
 # Property Schema
 class PropertySchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ( 'name', 'description', 'city', 'room', 'userid')
+        fields = ( 'name', 'description', 'city', 'room', 'owner_id')
